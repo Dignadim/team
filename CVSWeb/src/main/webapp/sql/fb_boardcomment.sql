2d454325be825bca4e9e84579f7358e851458fb3
Choi Sung Min <minijjang9@naver.com>
2022-11-13 16:29:22 +0900

    1112백섭

---
+++ CVSWeb/src/main/webapp/sql/fb_boardcomment.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "TEAM"."FB_BOARDCOMMENT" (
+    "FBC_IDX" NUMBER NOT NULL ENABLE, 
+	"FBC_GUP" NUMBER, 
+	"FBC_CONTENT" VARCHAR2(2000 BYTE) NOT NULL ENABLE, 
+	"FBC_DATE" DATE DEFAULT sysdate, 
+	 CONSTRAINT "FB_BOARDCOMMENT_PK" PRIMARY KEY ("FBC_IDX")
+   );
+   
+delete from fb_boardcomment;
+drop sequence fb_boardcomment_idx_seq;
+create sequence fb_boardcomment_idx_seq;
+
+insert into fb_boardcomment (fbc_idx, fbc_gup, fbc_content) 
+    values (fb_boardcomment_idx_seq.nextval, 0, '룰루');
+
+select * from fb_boardcomment;
+select count(*) from fb_boardcomment
+
+commit;
