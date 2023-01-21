c3cf9de060191c1340e04dbc95e17c87aeed8762
Choi SungMin <minijjang9@naver.com>
2023-01-21 22:18:59 +0900

    no message

--- CVSWeb/src/main/webapp/WEB-INF/js/itemImageInsertOK.js
+++
@@ -1,9 +0,0 @@
-function thisClose() {
-	let itemImage = document.getElementsByName('itemImage')[0].value
-	let fileRealname = document.getElementsByName('fileRealname')[0].value
-	
-	opener.document.getElementById('itemImage').value = itemImage;
-	opener.document.getElementById('fileRealname').value = fileRealname;	
-	
-	self.close();
-}
